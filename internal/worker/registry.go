a42b2164ff8d6cc08276c970822589b2d3d6d8f6
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-12-03 18:28:57 +0700

    feat: add handlerregistry

---
+++ internal/worker/registry.go
@@ -0,0 +1,29 @@
+package worker
+
+import (
+	"context"
+	"sync"
+)
+
+type HandlerFunc func(context.Context) error
+
+type HandlerRegistry struct {
+	// use RWMutex instead of Mutex, because it won't block read if not writing.
+	// Also for case when read is more frequent than writing
+	mu sync.RWMutex
+
+	handlers map[string]HandlerFunc // map is not concurrent safe, so add mutex
+}
+
+func (r *HandlerRegistry) Register(taskType string, fun HandlerFunc) {
+	r.mu.Lock() // exclusive writes, only one goroutine can modify at a time
+	r.handlers[taskType] = fun
+	r.mu.Unlock()
+}
+
+func (r *HandlerRegistry) Get(taskType string) (HandlerFunc, bool) {
+	r.mu.RLock() // concurrent reads, multiple goroutines can read simultaneously
+	fun, ok := r.handlers[taskType]
+	r.mu.RUnlock()
+	return fun, ok
+}
